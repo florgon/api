5e24923413f01ba4b6df85fb7986b3dbe687101b
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-04-07 09:05:22 +0400

    Gin simple setup.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,30 @@
-package main;
+package main
 
-import "fmt";
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func index(c *gin.Context) {
+	// TODO: Encapsulate struct / response logic.
+	var message struct {
+		Status  int    `json:"status"`
+		Version string `json:"v"`
+		Payload struct {
+			Methods []string `json:"methods"`
+		} `json:"success"`
+	}
+
+	message.Status = http.StatusOK
+	message.Version = "v0.1 DEV"
+	message.Payload.Methods = []string{"/"}
+	c.JSON(message.Status, message)
+}
 
 func main() {
-	fmt.Println("Hello from Florgon API authentication server!");
+	r := gin.Default()
+
+	r.GET("/", index)
+	r.Run()
 }
