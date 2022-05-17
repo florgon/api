92501e19746ed1319630949a8d53516461347ab3
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-05-18 00:06:31 +0400

    User add avatar, first, last names and sex database fields.

--- src/auth-api/app/database/models/user.py
+++ src/auth-api/app/database/models/user.py
@@ -16,16 +16,32 @@ class User(Base):
     """ Auth service user model"""
     __tablename__ = "users"
 
+    # Database.
     id = Column(Integer, primary_key=True, index=True, nullable=False)
 
-    username = Column(String, unique=True, index=True, nullable=False)
+
+    # Sensitive information.
     email = Column(String, unique=True, index=True, nullable=False)
 
-    password = Column(String, nullable=False)
+    # Display.
+    username = Column(String, unique=True, index=True, nullable=False)
+    avatar = Column(String, nullable=True)
 
+    # Name.
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+
+    # Other.
+    sex = Column(Boolean, nullable=False, default=False)
+    
+    # Password.
+    password = Column(String, nullable=False)
+    
+    # States.
     is_active = Column(Boolean, nullable=False, default=True)
     is_verified = Column(Boolean, nullable=False, default=False)
 
+    # Times.
     time_created = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
     time_verified = Column(DateTime(timezone=True), nullable=True)
