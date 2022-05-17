08349b1242e9da074ae1eabe66ee0f200422c788
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-05-17 23:28:04 +0400

    Fix OAuth clients being verified by default.

--- src/auth-api/app/database/models/oauth_client.py
+++ src/auth-api/app/database/models/oauth_client.py
@@ -31,7 +31,7 @@ class OAuthClient(Base):
     is_active = Column(Boolean, nullable=False, default=True)
 
     # Means is client verified by administrators (allow access to direct auth later)
-    is_verified = Column(Boolean, nullable=False, default=True)
+    is_verified = Column(Boolean, nullable=False, default=False)
 
     time_created = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
