cc5f0bf8157d7e33aa8520c0e1c4ae7712efa633
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-06-08 11:48:34 +0400

    Do not create new session if authorizing with queried session client data

--- src/api/app/database/crud/user_session.py
+++ src/api/app/database/crud/user_session.py
@@ -18,7 +18,7 @@ def generate_secret() -> str:
     return secrets.token_urlsafe(32)
 
 
-def get_by_id(db: Session, session_id: int) -> UserSession:
+def get_by_id(db: Session, session_id: int) -> UserSession | None:
     """ Returns session by it`s ID. """
     return db.query(UserSession).filter(UserSession.id == session_id).first()
 
@@ -26,12 +26,24 @@ def get_by_id(db: Session, session_id: int) -> UserSession:
 def get_by_owner_id(db: Session, owner_id: int) -> list[UserSession]:
     return db.query(UserSession).filter(UserSession.owner_id == owner_id).all()
 
-def create(db: Session, owner_id: int, client_host: str, client_user_agent: str) -> UserSession:
-    """Creates user session"""
+
+def get_by_ip_address_and_user_agent(db: Session, ip_address: str, user_agent: UserAgent) -> UserSession | None:
+    return db.query(UserSession).\
+        filter(UserSession.ip_address == ip_address).\
+        filter(UserSession.user_agent_id == user_agent.id).\
+            first()
+
+
+def get_or_create_new(db: Session, owner_id: int, client_host: str, client_user_agent: str) -> UserSession:
+    """Returns user session or creates a new one."""
 
     # Query user agent.
     user_agent: UserAgent = crud.user_agent.get_or_create_by_string(db, client_user_agent)
     user_agent_id = user_agent.id
+
+    queried_session: UserSession = get_by_ip_address_and_user_agent(db, client_host, user_agent)
+    if queried_session and queried_session.owner_id == owner_id:
+        return queried_session
 
     # Create new user session.
     session_token_secret = generate_secret()
