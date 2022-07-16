9fa756721aea859fd9828502efad5b3d34dfbd23
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-16 19:39:07 +0400

    Fix admin router was not included.

--- src/api/app/routers/__init__.py
+++ src/api/app/routers/__init__.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI
 
 from app.config import get_settings
 
-from . import email, oauth_client, secure, oauth, session, user, utils, ext_social_auth
+from . import email, oauth_client, secure, oauth, session, user, utils, ext_social_auth, admin
 
 
 def register_routers(app: FastAPI) -> None:
@@ -23,5 +23,6 @@ def register_routers(app: FastAPI) -> None:
         utils.router,
         secure.router,
         ext_social_auth.router,
+        admin.router,
     ]:
         app.include_router(router, prefix=get_settings().proxy_url_prefix)
