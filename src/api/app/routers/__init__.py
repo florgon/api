e1854efe518990ed42ad972b26df1f31662b35a7
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-19 15:11:51 +0400

    Security boilerplate router methods.
    Refers to #61 and #63

--- src/api/app/routers/__init__.py
+++ src/api/app/routers/__init__.py
@@ -17,6 +17,7 @@ from . import (
     utils,
     ext_social_auth,
     admin,
+    security,
 )
 
 
@@ -34,5 +35,6 @@ def register_routers(app: FastAPI) -> None:
         secure.router,
         ext_social_auth.router,
         admin.router,
+        security.router
     ]:
         app.include_router(router, prefix=get_settings().proxy_url_prefix)
