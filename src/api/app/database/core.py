c71f473872cefc743173b108b203c847ca955d27
Kirill Zhosul <kirillzhosul@yandex.com>
2022-07-21 12:17:53 +0400

    Pool size. (#98)

--- src/api/app/database/core.py
+++ src/api/app/database/core.py
@@ -7,12 +7,18 @@ from sqlalchemy import MetaData
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import QueuePool
 
 # Settings.
 from app.config import Settings
 
 # Database engine.
-engine = create_engine(Settings().database_url)
+engine = create_engine(url=Settings().database_url,
+                       pool_size=20, 
+                       max_overflow=0, 
+                       pool_recycle=3600, 
+                       poolclass=QueuePool
+)
 metadata = MetaData(bind=engine)
 
 # Base, session from core.
