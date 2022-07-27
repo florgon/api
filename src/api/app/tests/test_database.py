b7d33d62f388156e156feaca3ea1b4fcf866dce4
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-27 23:59:49 +0400

    Add database test.

---
+++ src/api/app/tests/test_database.py
@@ -0,0 +1,19 @@
+import pytest
+
+from fastapi.testclient import TestClient
+
+from app.app import app
+from app.database.dependencies import get_db, Session
+
+@pytest.fixture
+def client():
+    with TestClient(app) as c:
+        yield c
+
+
+def test_read_utils_get_server_time(_):
+    db = get_db()
+    expected = 32
+    with db.begin() as session:
+        session: Session = session
+        assert session.execute(f"SELECT {expected};") == expected
