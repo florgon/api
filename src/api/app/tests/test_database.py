72c11164434ba3177e0c0dcd16674e4151b17d4e
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-28 00:07:17 +0400

    Fix tests database session.

--- src/api/app/tests/test_database.py
+++ src/api/app/tests/test_database.py
@@ -3,17 +3,14 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app.app import app
-from app.database.dependencies import get_db, Session
+from app.database.core import SessionLocal
 
 @pytest.fixture
 def client():
     with TestClient(app) as c:
         yield c
 
-
 def test_database_sql_select(client):
-    db = get_db()
     expected = 32
-    with db.begin() as session:
-        session: Session = session
+    with SessionLocal as session:
         assert session.execute(f"SELECT {expected};") == expected
