53d5cd1fd5ed7fbc6faaa69100123785b6a9cc19
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-28 00:14:25 +0400

    Fix select SQL execution.

--- src/api/app/tests/test_database.py
+++ src/api/app/tests/test_database.py
@@ -1,6 +1,7 @@
 import pytest
 
 from fastapi.testclient import TestClient
+from sqlalchemy import text
 
 from app.app import app
 from app.database.core import SessionLocal
@@ -13,4 +14,4 @@ def client():
 def test_database_sql_select(client):
     expected = 32
     with SessionLocal() as session:
-        assert session.execute(f"SELECT {expected};") == expected
+        assert int(session.execute(text(f"SELECT {expected}")).fetchone()) == expected
