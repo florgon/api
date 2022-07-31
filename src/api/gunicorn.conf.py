d7bfe644eead9dcc8c348578e8e1723679a6c060
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-07-31 14:02:09 +0400

    Gunicorn enable span threads to CPU cores.

--- src/api/gunicorn.conf.py
+++ src/api/gunicorn.conf.py
@@ -1,9 +1,9 @@
 """
     Gunicorn process manager configuration.
 """
+from multiprocessing import cpu_count
 
 # TODO: `.env` support?
-# TODO: Automatically limit workers to cpu count. (from multiprocessing import cpu_count)
 
 proc_name = "florgon-api"
 
@@ -16,6 +16,7 @@ print_config = False
 
 forwarded_allow_ips = "['127.0.0.1']"
 
-threads = 1
+threads_span_to_cores = True
+threads = cpu_count if threads_span_to_cores else 1
 workers = 4
 worker_class = "uvicorn.workers.UvicornWorker"
