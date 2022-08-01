0e7a918f9b4ac5f9427557aade3489a239a18082
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-08-01 11:59:17 +0400

    Fix black.

--- src/api/app/tests/test_database.py
+++ src/api/app/tests/test_database.py
@@ -10,9 +10,10 @@ from app.database.core import SessionLocal
 
 
 class TestDatabaseUnit(unittest.TestCase):
-    """Check database unit (SQL) """
+    """Check database unit (SQL)"""
+
     def test_database_sql_select(self):
-        """Checks that SQL SELECT equals to argument number. """
+        """Checks that SQL SELECT equals to argument number."""
         expected = 32
         with SessionLocal() as session:
             actual = session.execute(text(f"SELECT {expected}")).fetchall()[0][0]
