54ad4d956c734e9c8b38a4b228481ad5f9842c52
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-08-02 22:51:27 +0400

    Publish refresh token with authorization code flow, add refresh token grant type.

--- src/api/app/tokens/refresh_token.py
+++ src/api/app/tokens/refresh_token.py
@@ -19,10 +19,20 @@ class RefreshToken(BaseToken):
 
     # Custom payload fields.
     _session_id: int = None
+    _client_id: int = None
+    _scope: str = ""
 
     def get_session_id(self) -> int:
-        """Returns session ID linked to the token."""
+        """Returns session ID of the token."""
         return self._session_id
+
+    def get_scope(self) -> str:
+        """Returns permissions scope of the token."""
+        return self._scope
+
+    def get_client_id(self) -> int:
+        """Returns the client ID linked for code."""
+        return self._client_id
 
     def __init__(
         self,
@@ -30,12 +40,16 @@ class RefreshToken(BaseToken):
         ttl: int | float,
         user_id: int,
         session_id: int | None = None,
+        scope: str | None = None,
+        client_id: int | None = None,
         payload: dict | None = None,
         *,
         key: str | None = None
     ):
         super().__init__(issuer, ttl, subject=user_id, payload={}, key=key)
         self._session_id = session_id
+        self._scope = scope
+        self._client_id = client_id
 
     @classmethod
     def decode(cls, token: str, key: str | None = None):
@@ -43,8 +57,14 @@ class RefreshToken(BaseToken):
         Decoding with custom payload fields.
         """
         instance = super(RefreshToken, cls).decode(token, key)
+
         session_id = instance._raw_payload["sid"]  # pylint: disable=protected-access
         instance._session_id = session_id  # pylint: disable=protected-access
+        scope = instance._raw_payload["scope"]  # pylint: disable=protected-access
+        instance._scope = scope  # pylint: disable=protected-access
+        client_id = instance._raw_payload["cid"]  # pylint: disable=protected-access
+        instance._client_id = client_id  # pylint: disable=protected-access
+
         return instance
 
     def encode(self, *, key: str | None = None) -> str:
@@ -52,4 +72,6 @@ class RefreshToken(BaseToken):
         Encodes token with custom payload fields.
         """
         self.custom_payload["sid"] = self._session_id
+        self.custom_payload["scope"] = self._scope
+        self.custom_payload["cid"] = self._client_id
         return super().encode(key=key)
