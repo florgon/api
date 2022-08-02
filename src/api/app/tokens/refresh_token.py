576f1df40b49dceb5181e60594bc0734bcff967f
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-08-02 22:30:56 +0400

    Create refresh_token.py

---
+++ src/api/app/tokens/refresh_token.py
@@ -0,0 +1,55 @@
+"""
+    Florgon API refresh token implementation.
+"""
+
+from .base_token import BaseToken
+
+
+class RefreshToken(BaseToken):
+    """
+    Refresh token JWT implementation.
+
+    Used for main authorization,
+    provides access to generate (publish) new access tokens after old ones expires.
+    Issued with OAuth flow.
+    Linked with session.
+    """
+
+    _type = "refresh"
+
+    # Custom payload fields.
+    _session_id: int = None
+
+    def get_session_id(self) -> int:
+        """Returns session ID linked to the token."""
+        return self._session_id
+
+    def __init__(
+        self,
+        issuer: str,
+        ttl: int | float,
+        user_id: int,
+        session_id: int | None = None,
+        payload: dict | None = None,
+        *,
+        key: str | None = None
+    ):
+        super().__init__(issuer, ttl, subject=user_id, payload={}, key=key)
+        self._session_id = session_id
+
+    @classmethod
+    def decode(cls, token: str, key: str | None = None):
+        """
+        Decoding with custom payload fields.
+        """
+        instance = super(RefreshToken, cls).decode(token, key)
+        session_id = instance._raw_payload["sid"]  # pylint: disable=protected-access
+        instance._session_id = session_id  # pylint: disable=protected-access
+        return instance
+
+    def encode(self, *, key: str | None = None) -> str:
+        """
+        Encodes token with custom payload fields.
+        """
+        self.custom_payload["sid"] = self._session_id
+        return super().encode(key=key)
