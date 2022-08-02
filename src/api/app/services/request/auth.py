9e2ffe6ca1a4f9536641c8de5e7f69cf92c3543e
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-08-03 02:31:56 +0400

    Refactor auth request handler, rework errors, permissions, checks.

--- src/api/app/services/request/auth.py
+++ src/api/app/services/request/auth.py
@@ -5,6 +5,7 @@
 """
 
 from typing import Type
+from datetime import datetime
 
 from app.database import crud
 from app.database.models.user_session import UserSession
@@ -27,6 +28,7 @@ def query_auth_data_from_token(
     required_permissions: list[Permission] | None = None,
     allow_deactivated: bool = False,
     allow_external_clients: bool = False,
+    trigger_online_update: bool = True,
     request: Request | None = None,
 ) -> AuthData:
     """
@@ -51,11 +53,13 @@ def query_auth_data_from_token(
     )
     if only_session_token:
         if auth_data.token.get_type() != SessionToken.get_type():
-            raise ApiErrorException(
-                ApiErrorCode.AUTH_INVALID_TOKEN,
-                "Got unexpected token type after decoding! Mostly due to token / server side error!",
-            )
-    return _query_auth_data(auth_data, db, allow_deactivated)
+            _raise_integrity_check_error()
+    return _query_auth_data(
+        auth_data,
+        db,
+        allow_deactivated=allow_deactivated,
+        trigger_online_update=trigger_online_update,
+    )
 
 
 def query_auth_data_from_request(
@@ -66,6 +70,7 @@ def query_auth_data_from_request(
     required_permissions: list[Permission] | None = None,
     allow_deactivated: bool = False,
     allow_external_clients: bool = False,
+    trigger_online_update: bool = True,
 ) -> AuthData:
     """
     Queries authentication data from request (from request token).
@@ -85,6 +90,7 @@ def query_auth_data_from_request(
         required_permissions=required_permissions,
         allow_deactivated=allow_deactivated,
         allow_external_clients=allow_external_clients,
+        trigger_online_update=trigger_online_update,
         request=req,
     )
 
@@ -119,7 +125,7 @@ def try_query_auth_data_from_request(
         )
         return True, auth_data
     except ApiErrorException:
-        # Got exception, not authorized.
+        # Any exception occurred - unable to authorize.
         return False, None
 
 
@@ -127,13 +133,20 @@ def _get_token_from_request(req: Request, only_session_token: bool) -> str:
     """
     Returns token from request.
     :param req: Request itself.
-    :param only_session_token: If true, will get session token.
+    :param only_session_token: If true, will get only session token.
     """
+
     if only_session_token:
+        # If we are requested to get only session token.
+
+        # Session token not expected to be as header, return just params check.
         return req.query_params.get("session_token", "")
-    return req.headers.get("Authorization", "") or req.query_params.get(
-        "access_token", ""
-    )
+
+    # Simple access token located in header and params.
+    # Notice that if user gives header and param, header should taken and param should skiped!
+    token_header = req.headers.get("Authorization", "")
+    token_param = req.query_params.get("access_token", "")
+    return token_header or token_param
 
 
 def _decode_token(
@@ -179,9 +192,11 @@ def _decode_token(
     )
     signed_token = token_type.decode(token, key=session.token_secret)
     if not signed_token.signature_is_valid():
+        # If there is invalid signature on the token,
+        # means token signed with another user, or old signature...
         raise ApiErrorException(
             ApiErrorCode.AUTH_INVALID_TOKEN,
-            "Token invalid! (Signature validation failed)",
+            "Unable to validate signature of the token!",
         )
 
     # Return DTO.
@@ -189,84 +204,138 @@ def _decode_token(
 
 
 def _query_scope_permissions(
-    scope: str, required_permissions: list[Permission] | None
+    scope: str, required_permissions: list[Permission] | Permission | None = None
 ) -> list[Permission]:
     """
-    Queries scope permissions with checking required permission.
+    Queries scope permissions with checking required permission (if passed).
     :param scope: Scope string (From request).
-    :param required_permissions: Permission to require.
+    :param required_permissions: Permissions to require, or just one permission, or no permissions.
     """
-    permissions = parse_permissions_from_scope(scope)
+    permissions: list[Permission] = parse_permissions_from_scope(scope)
 
-    if required_permissions:
-        if isinstance(required_permissions, Permission):
-            required_permissions = [required_permissions]
-        for required_permission in required_permissions:
-            if required_permission not in permissions:
-                raise ApiErrorException(
-                    ApiErrorCode.AUTH_INSUFFICIENT_PERMISSIONS,
-                    f"Insufficient permissions (required: {required_permission.value})",
-                    {"required_scope": required_permission.value},
-                )
+    if not required_permissions:
+        # If no permissions that should be required,
+        # simply return parsed permissions.
+        return permissions
+
+    if isinstance(required_permissions, Permission):
+        # If specified only one permission,
+        # convert it to list as expected.
+        required_permissions = [required_permissions]
+
+    # Filter scope permissions, and build list with only those permissions that not satisfied.
+    unsatisfied_permissions = list(
+        filter(lambda permission: permission not in permissions, required_permissions)
+    )
+
+    if unsatisfied_permissions:
+        # If we have any permission that not satisfied.
+
+        # String of scope of required permissions.
+        required_scope = ", ".join(
+            [permission.value for permission in unsatisfied_permissions]
+        )
+
+        raise ApiErrorException(
+            ApiErrorCode.AUTH_INSUFFICIENT_PERMISSIONS,
+            f"Insufficient permissions to call this method! (required scope permissions: {required_scope})",
+            {"required_scope": required_scope},
+        )
 
     return permissions
 
 
 def _query_session_from_sid(
-    session_id: int | None,
+    session_id: int,
     db: Session,
     request: Request | None = None,
     allow_external_clients: bool = False,
 ) -> UserSession:
     """
     Queries session from SID (session_id).
-    :param session_id: SID itself.
+    :param session_id: Session ID itself (SID).
     :param db: Database session.
+    :param request: Request for client check.
+    :param allow_external_clients: If false (default) will block all requests from suspicious devices.
     """
 
-    session = (
-        crud.user_session.get_by_id(db, session_id=session_id) if session_id else None
-    )
+    if not isinstance(session_id, int):
+        # Internal authentication system integrity check.
+        _raise_integrity_check_error()
 
-    # Validate session.
+    session = crud.user_session.get_by_id(db, session_id=session_id)
     if not session:
-        raise ApiErrorException(ApiErrorCode.AUTH_INVALID_TOKEN, "Token invalid!")
+        # Internal authentication system integrity check.
+        # users should never be deleted and this should never happen.
+        _raise_integrity_check_error()
+
     if not session.is_active:
+        # If session is not active anymore,
+        # means user closed (logouted) or whatever else happened.
         raise ApiErrorException(
             ApiErrorCode.AUTH_INVALID_TOKEN,
-            "Session closed (Token invalid due to session deactivation)!",
+            "Token is not usable as session was closed!",
         )
-    if request is not None and not allow_external_clients:
+
+    if not allow_external_clients and request is not None:
+        # If we are not expected to allow external clients (devices),
+        # trigger session suspicious check.
+        # this will disallow all suspicious devices that are different from session owner device.
         session_check_client_by_request(db, session, request)
+
     return session
 
 
 def _query_auth_data(
-    auth_data: AuthData, db: Session, allow_deactivated: bool
+    auth_data: AuthData,
+    db: Session,
+    allow_deactivated: bool = False,
+    trigger_online_update: bool = True,
 ) -> AuthData:
     """
-    Finalizes query of  authentication data.
-    :param auth_data: Authentication data.
+    Finalizes query of authentication data by query final user object.
+    :param auth_data: Authentication data DTO.
     :param db: Database session.
-    :param allow_deactivated: If true, allow deactivated user to authenticate.
+    :param allow_deactivated: If false, will raise API error for users that deactivated.
+    :param trigger_online_update: If true, will trigger online update for user.
     """
-    user = crud.user.get_by_id(db=db, user_id=auth_data.token.get_subject())
 
-    # Validate authentication data.
-    if not user:
+    # Query database for our user to feed into auth data DTO.
+    user_id = auth_data.token.get_subject()
+    user = crud.user.get_by_id(db=db, user_id=user_id)
+
+    if not user or auth_data.session.owner_id != user.id:
+        # Internal authentication system integrity check.
+        # users should never be deleted and this should never happen.
+        _raise_integrity_check_error()
+
+    if not user.is_active and not allow_deactivated:
+        # If user is deactivated (banned), and we are not allowed to return with deactivated users,
+        # return API error means deactivated user have no permissions to call requested method.
         raise ApiErrorException(
-            ApiErrorCode.AUTH_INVALID_CREDENTIALS,
-            "User with given token does not exists!",
-        )
-    if not allow_deactivated and not user.is_active:
-        raise ApiErrorException(
-            ApiErrorCode.USER_DEACTIVATED, "User account deactivated, access denied!"
-        )
-    if auth_data.session.owner_id != user.id:
-        raise ApiErrorException(
-            ApiErrorCode.AUTH_INVALID_TOKEN, "Token session was linked to another user!"
+            ApiErrorCode.USER_DEACTIVATED,
+            "User account currently deactivated and this method does not allow deactivated users!",
         )
 
-    # Return modified DTO.
+    if trigger_online_update:
+        # If this flag is true, means that we should do update of the online time for user.
+        # By default, this flag is enabled, means if you want to externally disable this trigger,
+        # you should know that this is external trigger.
+
+        # Do update of the online time for user.
+        user.time_online = datetime.now()
+        db.commit()
+
+    # Return modified DTO with user ORM model instance.
     auth_data.user = user
     return auth_data
+
+
+def _raise_integrity_check_error():
+    """
+    Raises authentication system integrity check error.
+    """
+    raise ApiErrorException(
+        ApiErrorCode.AUTH_INVALID_TOKEN,
+        "Authentication system integrity check failed!",
+    )
