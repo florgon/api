ef068596a60974ef1ffed14100bca5aea7bca68c
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-10-05 16:11:51 +0400

    Base boilerplate Gatey integration

--- src/api/app/middlewares/__init__.py
+++ src/api/app/middlewares/__init__.py
@@ -4,9 +4,25 @@
     And middlewares itself.
 """
 
-from app.config import get_settings
+from starlette.types import ASGIApp, Receive, Scope, Send
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.config import get_settings
+
+
+class GateyMiddleware:
+    """Gatey (error) logging middleware."""
+
+    def __init__(
+        self,
+        app: ASGIApp,
+    ) -> None:
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        await self.app(scope, receive, send)
+        return
 
 
 def add_middlewares(app: FastAPI) -> None:
@@ -14,6 +30,14 @@ def add_middlewares(app: FastAPI) -> None:
     Registers (add) all custom middlewares to the FastAPI application.
     """
     _add_cors_middleware(app)
+    _add_gatey_middleware(app)
+
+
+def _add_gatey_middleware(app: FastAPI) -> None:
+    """
+    Registers Gatey logging middleware.
+    """
+    app.add_middleware(GateyMiddleware)
 
 
 def _add_cors_middleware(app: FastAPI) -> None:
