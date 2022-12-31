0cfb6af2adadf73c31748bafb9ee35271b46104a
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-12-31 21:34:30 +0000

    Move Grafana + Prometheus into production docker env

--- src/api/app/event_handlers/__init__.py
+++ src/api/app/event_handlers/__init__.py
@@ -5,9 +5,13 @@
 """
 
 from fastapi import FastAPI
-from prometheus_fastapi_instrumentator import Instrumentator
+try:
+    from prometheus_fastapi_instrumentator import Instrumentator
+    prometheus_instrumentator_installed = False
+except ImportError:
+    prometheus_instrumentator_installed = False
 
-from app.config import get_settings
+from app.config import get_settings, get_logger
 from app.services import limiter
 
 
@@ -18,7 +22,10 @@ def add_event_handlers(app: FastAPI) -> None:
     app.add_event_handler("startup", limiter.on_startup)
     app.add_event_handler("shutdown", limiter.on_shutdown)
     if get_settings().prometheus_metrics_exposed:
-        app.add_event_handler(
-            "startup",
-            lambda: Instrumentator().instrument(app).expose(app),
-        )
+        if prometheus_instrumentator_installed:
+            app.add_event_handler(
+                "startup",
+                lambda: Instrumentator().instrument(app).expose(app),
+            )
+        else:
+            get_logger().warn("You are enabled `prometheus_metrics_exposed` but `prometheus_fastapi_instrumentator` is not installed in system!")
---
+++ src/docker/Production.Dockerfile
@@ -0,0 +1,21 @@
+# syntax=docker/dockerfile:1
+FROM python:3.10.7-alpine
+
+# Disable python buffering and bytecode *.pyc compiling. 
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+# Project directory.
+WORKDIR /srv/www/florgon/api
+
+# Install requirements.
+RUN apk add build-base
+RUN pip install --upgrade pip
+COPY api/requirements-production.txt /srv/www/florgon/api/
+RUN pip install --upgrade --no-cache-dir -r requirements-production.txt
+
+# Copy whole project.
+COPY . /srv/www/florgon/api/
+
+# Run project.
+CMD ["gunicorn", "app.app:app", "-c", "gunicorn.conf.py"]
