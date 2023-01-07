6ada4b11c67e3376a9db4b6081de1dc7e264f9c1
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2023-01-08 00:35:46 +0400

    Fix `app.services.request` `__init__`

--- src/api/app/services/request/__init__.py
+++ src/api/app/services/request/__init__.py
@@ -3,13 +3,25 @@
     validate session for client, get client host.
 """
 
-from .auth import get_token_from_request, query_auth_data_from_request
+from .auth import (
+    query_auth_data_from_request,
+    query_auth_data_from_token,
+    try_query_auth_data_from_request,
+    AuthDataDependency,
+    get_token_from_request,
+)
+from .auth_data import AuthData
 from .get_client_host import get_client_host_from_request
 from .session_check_client import session_check_client_by_request
 
+
 __all__ = [
-    "get_token_from_request",
-    "query_auth_data_from_request",
-    "session_check_client_by_request",
+    "AuthDataDependency",
+    "AuthData",
     "get_client_host_from_request",
+    "session_check_client_by_request",
+    "get_token_from_request",
+    "try_query_auth_data_from_request",
+    "query_auth_data_from_token",
+    "query_auth_data_from_request",
 ]
