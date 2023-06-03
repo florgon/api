9b4e12edf11074c6d669c395b8236630be651149
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2023-06-03 11:12:45 +0400

    refactor(routers): :art: split routers into v1, v2.
    Refactor routers to handle both old version v1 and new v2. Changes usage of proxy.
    
    Removes usage of proxy path.

--- src/api/app/routers/__init__.py
+++ src/api/app/routers/__init__.py
@@ -2,42 +2,15 @@
     FastAPI routers for application.
 """
 
-from app.config import get_settings
 from fastapi import FastAPI
 
-from . import (
-    admin,
-    email,
-    ext_oauth,
-    gift,
-    mailings,
-    oauth,
-    oauth_client,
-    secure,
-    security,
-    session,
-    user,
-    utils,
-)
+from .v2 import include_v2_routers
+from .v1 import include_v1_routers
 
 
 def include_routers(app: FastAPI) -> None:
     """
     Registers (Including) FastAPI routers for FastAPI app.
     """
-    proxy_url_prefix = get_settings().proxy_url_prefix
-    for module in [
-        oauth_client,
-        email,
-        session,
-        oauth,
-        user,
-        utils,
-        secure,
-        ext_oauth,
-        admin,
-        security,
-        gift,
-        mailings,
-    ]:
-        app.include_router(module.router, prefix=proxy_url_prefix)
+    include_v1_routers(app=app)
+    include_v2_routers(app=app)
