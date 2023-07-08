291af69067f948619c8a20d17a84c170bcc095b7
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2023-07-08 20:58:54 +0400

    fix(ext_oauth): :ambulance: Fix imports in ext oauth

--- src/api/app/services/ext_oauth/builders/__init__.py
+++ src/api/app/services/ext_oauth/builders/__init__.py
@@ -2,9 +2,9 @@
     Builders for OAuth services instances.
 """
 
-from yandex_builder import build_yandex_oauth_service
-from vk_builder import build_vk_oauth_service
-from github_builder import build_github_oauth_service
+from .yandex_builder import build_yandex_oauth_service
+from .vk_builder import build_vk_oauth_service
+from .github_builder import build_github_oauth_service
 
 __all__ = [
     "build_yandex_oauth_service",
